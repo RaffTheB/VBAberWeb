bd4eb8e0f340fb0b4caa38c59f8e5c087c335412
RaffTheB <r.barone97.03@gmail.com>
2025-07-10 22:18:36 +0200

    Update pwa.js

--- scripts/pwa.js
+++ scripts/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('scripts/service-worker.js')
       .then(reg => console.log('Service Worker registrato', reg))
       .catch(err => console.warn('Service Worker fallito', err));
   });
